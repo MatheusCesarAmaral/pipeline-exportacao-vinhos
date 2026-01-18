037a19a34357870f9382a159f006fae9c04e78ef
Matheus Amaral <matheuscesaramaral@hotmail.com>
2026-01-18 18:47:08 -0300

    extract revisado

--- src/extract/extract_embrapa.py
+++ src/extract/extract_embrapa.py
@@ -1,32 +1,47 @@
 import pandas as pd
 from pathlib import Path
+import logging
 
+# adicionando Logs
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-def extract_embrapa_online(url: str, output_path: Path) -> None:
-    print(f"Lendo dados da URL: {url}")
+# funcao para extrair os CSVs da Embrapa
+def extract_embrapa_online(url: str, output_path: Path, sep: str = ";", encoding: str = "latin1") -> bool:
+    """
+    extrai os dados da URL e salva localmente.
+    retorna True se tiver sucesso, False caso contrário.
+    """
+    try:
+        logging.info(f"Lendo dados da URL: {url}")
+        
+        # leitura dos dados
+        df = pd.read_csv(url, sep=sep, encoding=encoding)
+        
+        # garante que a pasta pai existe antes de salvar
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        
+        df.to_csv(output_path, index=False)
+        logging.info(f"Dados salvos com sucesso em: {output_path}")
+        return True
 
-    df = pd.read_csv(url, sep=";", encoding="latin1")
-
-    print(f"Salvando dados brutos em: {output_path}")
-    df.to_csv(output_path, index=False)
-
-    print("Ingestão concluída.\n")
-
+    except Exception as e:
+        logging.error(f"Erro ao processar {url}: {e}")
+        return False
 
 def main():
     urls = {
-        "producao": "https://vitibrasil.cnpuv.embrapa.br/download/Producao.csv",
         "comercio": "https://vitibrasil.cnpuv.embrapa.br/download/Comercio.csv",
         "exportacao": "https://vitibrasil.cnpuv.embrapa.br/download/ExpVinho.csv",
     }
 
     raw_dir = Path("data/raw")
-    raw_dir.mkdir(exist_ok=True, parents=True)
 
     for name, url in urls.items():
         output_file = raw_dir / f"{name}_raw.csv"
-        extract_embrapa_online(url, output_file)
-
+        sucesso = extract_embrapa_online(url, output_file)
+        
+        if sucesso:
+            print(f"Item '{name}' processado.\n")
 
 if __name__ == "__main__":
     main()
